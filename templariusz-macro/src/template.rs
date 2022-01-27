49132247b48ffca6ac988c4301e99890df8a77b1
michalwa <michalwa2003@gmail.com>
2022-01-27 13:39:21 +0100

    Rewrite tokenization & support else-if

--- templariusz-macro/src/template.rs
+++ templariusz-macro/src/template.rs
@@ -1,14 +1,16 @@
-use proc_macro2::{Ident, TokenStream};
+use proc_macro2::{Ident, TokenStream, TokenTree};
 use quote::quote;
 use std::str::FromStr;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum TemplateParseError {
-    #[error("Unmatched opening delimiter {0}")]
-    UnmatchedOpenDelim(String),
-    #[error("Unmatched `end'")]
-    UnmatchedEnd,
+    #[error("Unexpected token {0:?}")]
+    Unexpected(Token),
+    #[error("Unmatched delimiter {0}")]
+    Unmatched(String),
+    #[error("Empty block")]
+    EmptyBlock,
     #[error("{0}")]
     EvalError(#[from] proc_macro2::LexError),
 }
@@ -19,75 +21,13 @@ impl FromStr for Template {
     type Err = TemplateParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        enum Token {
-            Literal(String),
-            Eval(TokenStream),
-            BlockBegin(TokenStream),
-            BlockEnd,
-        }
-
-        let mut match_indices = s
-            .match_indices("{%")
-            .chain(s.match_indices("{{"))
-            .collect::<Vec<_>>();
-
-        match_indices.sort_by_key(|&(i, _)| i);
-
-        let mut tokens = vec![];
-        let mut last_end = 0;
-
-        for (start, open_delim) in match_indices {
-            if start > last_end {
-                let mut literal = s[last_end..start].to_string();
-                if literal.ends_with('\n') {
-                    literal.pop();
-                    if literal.ends_with('\r') { literal.pop(); }
-                }
-                tokens.push(Token::Literal(literal));
-            }
-
-            let close_delim = match open_delim {
-                "{%" => "%}",
-                "{{" => "}}",
-                _ => unreachable!(),
-            };
-
-            let len = s[start..]
-                .find(close_delim)
-                .ok_or(TemplateParseError::UnmatchedOpenDelim(open_delim.into()))?;
-            let end = start + len;
-            let inner = &s[(start + 2)..end];
-
-            match open_delim {
-                "{%" => {
-                    let keyword = inner.trim();
-                    if keyword == "end" {
-                        tokens.push(Token::BlockEnd);
-                    } else if keyword == "else" {
-                        tokens.push(Token::BlockEnd);
-                        tokens.push(Token::BlockBegin(inner.parse()?));
-                    } else {
-                        tokens.push(Token::BlockBegin(inner.parse()?));
-                    }
-                }
-                "{{" => tokens.push(Token::Eval(inner.parse()?)),
-                _ => unreachable!(),
-            }
-
-            last_end = end + 2;
-        }
-
-        if last_end < s.len() {
-            tokens.push(Token::Literal(s[last_end..].into()));
-        }
-
         let mut block_stack = vec![Block {
-            begin: TokenStream::new(),
+            begin: None,
             body: vec![],
         }];
 
-        for token in tokens {
-            match token {
+        for token in Self::tokenize(s)? {
+            match token.clone() {
                 Token::Literal(literal) => {
                     block_stack
                         .last_mut()
@@ -100,13 +40,30 @@ impl FromStr for Template {
                 }
                 Token::BlockBegin(begin) => {
                     block_stack.push(Block {
-                        begin,
+                        begin: Some(begin),
+                        body: vec![],
+                    });
+                }
+                Token::BlockContinue(begin) => {
+                    if block_stack.len() == 1 {
+                        return Err(TemplateParseError::Unexpected(token));
+                    }
+
+                    let block = block_stack.pop().unwrap();
+                    block_stack
+                        .last_mut()
+                        .unwrap()
+                        .body
+                        .push(Part::Block(block));
+
+                    block_stack.push(Block {
+                        begin: Some(begin),
                         body: vec![],
                     });
                 }
                 Token::BlockEnd => {
                     if block_stack.len() == 1 {
-                        return Err(TemplateParseError::UnmatchedEnd);
+                        return Err(TemplateParseError::Unexpected(token));
                     }
 
                     let block = block_stack.pop().unwrap();
@@ -124,6 +81,69 @@ impl FromStr for Template {
 }
 
 impl Template {
+    fn next_of<'a>(delims: &[&str], s: &'a str) -> Option<(usize, &'a str)> {
+        delims
+            .iter()
+            .filter_map(|d| s.match_indices(d).next())
+            .min_by_key(|&(i, _)| i)
+    }
+
+    fn tokenize(mut s: &str) -> Result<Vec<Token>, TemplateParseError> {
+        let mut tokens = vec![];
+
+        while let Some((start, open_delim)) = Self::next_of(&["{%", "{{"], s) {
+            if start > 0 {
+                let mut literal = s[..start].to_string();
+                if literal.ends_with('\n') {
+                    literal.pop();
+                    if literal.ends_with('\r') { literal.pop(); }
+                }
+                tokens.push(Token::Literal(literal));
+                s = &s[start..];
+            }
+
+            let (len, close_delim) = Self::next_of(&["%}", "}}"], s)
+                .ok_or_else(|| TemplateParseError::Unmatched(open_delim.into()))?;
+
+            let expected_close_delim = match open_delim {
+                "{%" => "%}",
+                "{{" => "}}",
+                _ => unreachable!(),
+            };
+
+            if close_delim != expected_close_delim {
+                return Err(TemplateParseError::Unmatched(open_delim.into()));
+            }
+
+            let inner = s[..len].strip_prefix(open_delim).unwrap();
+
+            match open_delim {
+                "{{" => tokens.push(Token::Eval(inner.parse()?)),
+                "{%" => {
+                    let inner_tokens: TokenStream = inner.parse()?;
+                    match inner_tokens
+                        .clone()
+                        .into_iter()
+                        .next()
+                        .ok_or(TemplateParseError::EmptyBlock)?
+                    {
+                        TokenTree::Ident(ident) => match ident.to_string().as_ref() {
+                            "end" => tokens.push(Token::BlockEnd),
+                            "else" => tokens.push(Token::BlockContinue(inner_tokens)),
+                            _ => tokens.push(Token::BlockBegin(inner_tokens)),
+                        }
+                        _ => tokens.push(Token::BlockBegin(inner_tokens)),
+                    }
+                }
+                _ => unreachable!(),
+            }
+
+            s = s[len..].strip_prefix(close_delim).unwrap();
+        }
+
+        Ok(tokens)
+    }
+
     pub fn emit_render(self, struct_name: Ident) -> TokenStream {
         let body = self.0.emit_render();
 
@@ -140,6 +160,15 @@ impl Template {
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum Token {
+    Literal(String),
+    Eval(TokenStream),
+    BlockBegin(TokenStream),    // if, for, while
+    BlockContinue(TokenStream), // else, else if
+    BlockEnd,                   // end
+}
+
 enum Part {
     Literal(String),
     Eval(TokenStream),
@@ -147,7 +176,7 @@ enum Part {
 }
 
 struct Block {
-    begin: TokenStream,
+    begin: Option<TokenStream>,
     body: Vec<Part>,
 }
 
--- templariusz/examples/basic.rs
+++ templariusz/examples/basic.rs
@@ -6,6 +6,7 @@ struct Greeting {
     items: Vec<String>,
     numbers: Vec<u32>,
     morning: bool,
+    evening: bool,
 }
 
 fn main() {
@@ -14,6 +15,7 @@ fn main() {
         items: vec!["Lorem ipsum".into(), "Foobar".into()],
         numbers: vec![42, 30, 7, 15],
         morning: false,
+        evening: true,
     };
     println!("{}", greeting.render());
 }
