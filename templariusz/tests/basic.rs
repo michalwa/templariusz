dc9b9ab4e1bd920a4d89b04755a9ad77423508f1
michalwa <michalwa2003@gmail.com>
2022-01-29 11:33:13 +0100

    Make basic example into a test

--- templariusz/tests/basic.rs
+++ templariusz/tests/basic.rs
@@ -1,4 +1,4 @@
-use templariusz::{Template, template};
+use templariusz::{template, Template};
 
 #[template(source = "{{ self.first_name }} {{ self.last_name }}")]
 struct Person {
@@ -6,11 +6,43 @@ struct Person {
     last_name: String,
 }
 
+#[template(path = "basic.template")]
+struct Greeting {
+    name: String,
+    items: Vec<String>,
+    numbers: Vec<u32>,
+    morning: bool,
+    evening: bool,
+}
+
 #[test]
-fn basic_substitution() {
+fn from_source() {
     let person = Person {
         first_name: "John".into(),
         last_name: "Doe".into(),
     };
     assert_eq!(person.render(), "John Doe");
+}
+
+#[test]
+fn from_file() {
+    let greeting = Greeting {
+        name: "John Doe".into(),
+        items: vec!["Lorem ipsum".into(), "Foobar".into()],
+        numbers: vec![42, 30, 7, 15],
+        morning: false,
+        evening: true,
+    };
+    assert_eq!(
+        greeting.render().lines().collect::<Vec<_>>(),
+        [
+            "Hello, John Doe!",
+            "Items:",
+            "    - Lorem ipsum",
+            "    - Foobar",
+            "Numbers: 42, 30, 7, 15, ",
+            "",
+            "    Good evening",
+        ],
+    )
 }
